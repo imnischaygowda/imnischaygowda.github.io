2d4603975cf3f35997dd4843c106983b4f137a39
Nischay Gowda <nischaygowda777@gmail.com>
2023-04-10 21:35:23 -0700

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -13,8 +13,8 @@ const exp = [
   },
   {
     title: "Junior Data Scientist",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
+    cardImage: "assets/images/experience-page/radome_tech.png",
+    place: "Radome Technologies and Services",
     time: "(Mar - Aug, 2019)",
     desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
   },
